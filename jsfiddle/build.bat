30ca92f46c182159cb4127f4041f5a6c21474860
unknown <tthogho1@gmail.com>
2024-04-14 16:48:05 +0900

    connect to websocket server

--- jsfiddle/build.bat
+++ jsfiddle/build.bat
@@ -4,3 +4,10 @@ set GOARCH=wasm
 go build -o dist/demo.wasm
 
 pause
+
+copy dist/demo.wasm C:\nginx-1.16.1\html
+copy demo.css C:\nginx-1.16.1\html
+copy offer.html C:\nginx-1.16.1\html
+copy wasm_exec.js C:\nginx-1.16.1\html
+
+pause
